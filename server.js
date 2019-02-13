27ab7d0d6747c5be804d2e34c2d95cd7d7b65ef9
Jalia Evans <irohdesign@outlook.com>
2019-02-13 10:54:54 -0500

    setting up MERN application and pushing to heroku

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    if (process.env.NODE_ENV === "production") {
+        app.use(express.static("client/build"));
+        const path = require('path');
+        app.get('*', (req, res) => {
+          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+      }
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 
+mongoose.connect(
+    process.env.MONDODB_URI || "mongodb://localhost/mern", {
+        useMongoClient: true
+    }
+);
